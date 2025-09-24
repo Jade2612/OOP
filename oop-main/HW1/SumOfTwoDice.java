94525aa481bfe2a448c5dc25f0def8ea7b59cd1e
Jade2612 <bcikdaongc@gmail.com>
2025-09-24 12:08:54 +0700

    Add files via upload

---
+++ oop-main/HW1/SumOfTwoDice.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+public class SumOfTwoDice {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int a = rand.nextInt(6) + 1;
+        int b = rand.nextInt(6) + 1;
+        System.out.println(a + b);
+    }
+}
