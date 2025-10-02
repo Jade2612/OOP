b2d0dd292b8dc1681bfceb4aaa9e18a8082acad7
Khoa Dao Ngoc  Bich <bcikdaongc@gmail.com>
2025-10-02 21:47:34 +0700

    Delete oop-main/HW1 directory

--- oop-main/HW1/RollDie.java
+++
@@ -1,9 +0,0 @@
-import java.util.Random;
-
-public class RollDie {
-    public static void main(String[] args) {
-        Random rand = new Random();
-        int num = rand.nextInt(6) + 1;
-        System.out.println(num);
-    }
-}
